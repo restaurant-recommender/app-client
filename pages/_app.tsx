634ae5ba6a415adacd3a05d74189befe8c028301
Chanchana Wicha <chanchana@wongnai.com>
2021-01-06 14:01:48 +0700

    Setup structure

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import "../styles/styles.scss";
+import "../styles/antd.less";
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <div className="app">
+    <Component {...pageProps} />
+  </div>
+}
---
+++ pages/register.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from 'antd';
+
+export default () => {
+  return (
+    <div className="container middle">
+      Test ja
+      <Button type="primary">Primary Button</Button>
+    </div>
+  )
+}
