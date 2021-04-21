e3231c41d9049827d95fbec23907b04b1bf35073
Chanchana Wicha <chanchana.w@lmwn.com>
2021-04-21 18:25:47 +0700

    Add og

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <title>Kinraidee Alpha</title>
+          <meta property="og:title" content="Kinraidee Alpha" key="title" />
           <meta name="description" content="Let Kinraidee help you find your perfect meal!"></meta>
           <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossOrigin=""/>
           <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
